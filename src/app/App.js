919c4c23fe49d96f91283842a95b37eb6d46c245
Corey Lutrick <coreylutrick@gmail.com>
2018-06-27 19:30:42 -0700

    got the mashups printing out on the left in animals component

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import AnimalForm from '../components/AnimalForm/AnimalForm';
 import Animals from '../components/Animals/Animals';
+import connection from '../firebaseRequests/connection';
+import animalRequests from '../firebaseRequests/animals';
 import './App.css';
 
-class App extends Component {
-  render () {
+class App extends Component
+{
+  state =
+  {
+    animals: [],
+  };
+  componentDidMount ()
+  {
+    connection();
+    animalRequests.getRequest()
+      .then((animals) =>
+      {
+        this.setState({animals});
+      })
+      .catch((err) =>
+      {
+        console.error(err);
+      });
+  }
+  render ()
+  {
     return (
       <div className="App">
         <div className="col-sm-6">
-          <Animals />
+          <Animals animals={this.state.animals} />
         </div>
         <div className="col-sm-6">
           <AnimalForm />
